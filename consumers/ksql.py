5a449173b402298dc1661b00edf83105305c451b
dipanjan saha <dipanjansaha@mac-v67kgtfl.ascorp.com>
2020-02-12 11:44:26 +0530

    KSQL Table configuration complete
    consumers/ksql.py -Done

--- consumers/ksql.py
+++ consumers/ksql.py
@@ -23,14 +23,19 @@ KSQL_URL = "http://localhost:8088"
 
 KSQL_STATEMENT = """
 CREATE TABLE turnstile (
-    ???
+    station_id INT,
+    station_name VARCHAR,
+    line VARCHAR
 ) WITH (
-    ???
+    KAFKA_TOPIC = 'org.chicago.cta.turnstile',
+    VALUE_FORMAT = 'avro',
+    KEY = 'station_id'
 );
-
 CREATE TABLE turnstile_summary
-WITH (???) AS
-    ???
+WITH (VALUE_FORMAT = 'JSON') AS
+    SELECT station_id, COUNT(station_id) AS count 
+    FROM turnstile 
+    GROUP BY station_id;
 """
 
 
@@ -58,3 +63,4 @@ def execute_statement():
 
 if __name__ == "__main__":
     execute_statement()
+
